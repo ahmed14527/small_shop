a68de51e15a1ee348d3162e17f25a6ef6be9706c
Bin Shawky <108636071+ahmed14527@users.noreply.github.com>
2024-06-24 04:27:21 +0300

    add

---
+++ order/serialziers.py
@@ -0,0 +1,30 @@
+
+from rest_framework import serializers
+from .models import CartItem
+from products.serializers import ProductListSerializer
+
+
+class AddToCartSerializer(serializers.Serializer):
+    product_id = serializers.IntegerField(required=True)
+    quantity = serializers.IntegerField(required=True)
+
+
+class UpdateCartItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CartItem
+        fields = ("quantity",)
+
+
+class CartItemListSerializer(serializers.ModelSerializer):
+    product = ProductListSerializer()
+
+    class Meta:
+        model = CartItem
+        exclude = ('user',)
+
+
+
+class OrderCreateSerializer(serializers.Serializer):
+    cart_items = serializers.ListField(child=serializers.IntegerField())
+    user_address = serializers.IntegerField(required=True)
+    subtotal_price = serializers.FloatField(required=True)
---
+++ order/views.py
@@ -0,0 +1,75 @@
+
+from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.generics import UpdateAPIView, DestroyAPIView, ListAPIView, CreateAPIView
+from .serialziers import AddToCartSerializer, UpdateCartItemSerializer, CartItemListSerializer, OrderCreateSerializer
+from .models import CartItem, Order
+from django.shortcuts import get_object_or_404
+from products.models import Product
+
+class AddToCartView(APIView):
+    serializer_class = AddToCartSerializer
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        try:
+            serializer = self.serializer_class(data=request.data)
+            if not serializer.is_valid():
+                return Response(data={"message": "invalid_data"}, status=status.HTTP_400_BAD_REQUEST)
+            data = serializer.validated_data
+            product = Product.objects.get(id=data.get("product_id"))
+            item = CartItem.objects.create(user=request.user, quantity=data.get("quantity"), product=product)
+
+            return Response(data={"message": "cart_item_added", "result": {"cart_item_id": item.id}})
+        except Product.DoesNotExist:
+            return Response(data={"message": "product_not_found"}, status=status.HTTP_404_NOT_FOUND)
+
+
+class UpdateUserCartItem(UpdateAPIView):
+    queryset = CartItem.objects.all()
+    serializer_class = UpdateCartItemSerializer
+    lookup_field = 'product_id'
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        cart_item = self.queryset.get(user=self.request.user, product_id=self.kwargs.get(self.lookup_field))
+        return cart_item
+
+
+class DeleteUserCartItemView(DestroyAPIView):
+    queryset = CartItem.objects.all()
+    lookup_field = 'id'
+    permission_classes = [IsAuthenticated]
+    serializer_class = UpdateCartItemSerializer
+
+
+class CartItemsListView(ListAPIView):
+    queryset = CartItem.objects.all()
+    serializer_class = CartItemListSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user)
+
+
+class OrderCreateView(CreateAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderCreateSerializer
+    permission_classes = [IsAuthenticated]
+
+    def create(self, request, *args, **kwargs):
+        try:
+            serializer = self.serializer_class(data=request.data)
+            if not serializer.is_valid():
+                return Response(data={"message": "invalid_data"}, status=status.HTTP_400_BAD_REQUEST)
+            address = address.objects.get(id=serializer.validated_data.get("user_address"))
+            cart_items = CartItem.objects.filter(id__in=serializer.validated_data.get("cart_items"))
+            total_price = sum([item.product.price for item in cart_items])
+            order = Order.objects.create(user=request.user, address=address, total_price=total_price)
+            return Response(data={"message": "order_created", "result": {"order_id": order.id}})
+
+        except Exception as e:
+            return Response(data={"message": "error", "result": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
